fe47ee15b4525e93c27c80e306067a6774625052
Sandipan Jha <sandipanjha3@gmail.com>
2025-04-16 00:13:43 +0530

    Routes created

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import { authRoutes } from './routes/auth.js';
+import { tasksRoutes } from './routes/tasks.js';
 
 const app = express();
 
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('Running')
-})
+// app.get('/',(req,res)=>{
+//     res.send('Running')
+// })
 
-app.listen(port)
+app.use('/', authRoutes);
+
+app.use('/tasks', tasksRoutes);
+
+
+app.listen(port,()=>{
+    console.log(`Server running at port : ${port}`);
+    
+})
